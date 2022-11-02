e04a7a8f2840a741a14f638b261d1d72962ebb43
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-11-02 11:31:13 +0700

    Use Page Parent

--- pager.php
+++ pager.php
@@ -1,5 +1,5 @@
 <?php if ($prev = $pager->prev): ?>
-  <a href="<?= $prev->link ?? $prev->url ?? ""; ?>" rel="prev" title="<?= i('Previous'); ?>">
+  <a href="<?= $prev->link ?? $prev->url; ?>" rel="prev" title="<?= i('Previous'); ?>">
     &#x25C0;
   </a>
 <?php else: ?>
@@ -7,17 +7,23 @@
     &#x25C0;
   </a>
 <?php endif; ?>
-<?php if ($parent = $pager->parent): ?>
-  <a href="<?= $parent->link ?? $parent->url ?? ""; ?>" title="<?= i('Parent'); ?>">
+<?php if ($parent = $page->parent): ?>
+  <a href="<?= $parent->url; ?>" title="<?= i('Parent'); ?>">
     &#x25C6;
   </a>
 <?php else: ?>
-  <a aria-disabled="true" title="<?= i('Parent'); ?>">
-    &#x25C6;
-  </a>
+  <?php if ($site->is('home')): ?>
+    <a aria-disabled="true" title="<?= i('Parent'); ?>">
+      &#x25C6;
+    </a>
+  <?php else: ?>
+    <a href="<?= $url; ?>" title="<?= i('Home'); ?>">
+      &#x25C6;
+    </a>
+  <?php endif; ?>
 <?php endif; ?>
 <?php if ($next = $pager->next): ?>
-  <a href="<?= $next->link ?? $next->url ?? ""; ?>" rel="next" title="<?= i('Next'); ?>">
+  <a href="<?= $next->link ?? $next->url; ?>" rel="next" title="<?= i('Next'); ?>">
     &#x25B6;
   </a>
 <?php else: ?>
