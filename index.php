b2a691156753bb699926e76caf55f2cb00df0e10
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-02-15 21:46:06 +0700

    Name-Space

--- index.php
+++ index.php
@@ -1,48 +1,54 @@
-<?php
+<?php namespace y\log;
+
+// Create site link data to be used in navigation
+function links(string $folder) {
+    \extract(\lot(), \EXTR_SKIP);
+    $r = [];
+    $route_current = $url->path . '/';
+    $route_index = '/' . \trim($state->route ?? 'index', '/');
+    foreach (\g($folder, 'page') as $k => $v) {
+        $v = new \Page($k);
+        // Exclude home page
+        if ($route_index === ($route = $v->route)) {
+            continue;
+        }
+        // Add current state
+        $v->current = 0 === \strpos($route_current, $route . '/');
+        $r[$v->title . \P . $k] = $v;
+    }
+    \ksort($r);
+    return \array_values($r);
+}
+
+// Create site trace data to be used in navigation
+function traces(string $folder) {
+    \extract(\lot(), \EXTR_SKIP);
+    $chops = \explode('/', \trim($url->path ?? "", '/'));
+    $r = [];
+    while ($chop = \array_shift($chops)) {
+        $folder .= \D . $chop;
+        if ($file = \exist([
+            $folder . '.archive',
+            $folder . '.page'
+        ], 1)) {
+            $r[] = $file;
+        }
+    }
+    return $r;
+}
 
 // Set page `type` to `Markdown` by default
-if (null !== State::get('x.markdown') && !State::get('x.page.page.type')) {
-    State::set('x.page.page.type', 'Markdown');
+if (null !== \State::get('x.markdown') && !\State::get('x.page.page.type')) {
+    \State::set('x.page.page.type', 'Markdown');
 }
 
 // Add CSS file to the `<head>` section…
-if (defined('TEST') && TEST) {
-    Asset::set('index.css', 20);
+if (\defined("\\TEST") && \TEST) {
+    \Asset::set('index.css', 20);
 } else {
     // Serve the minified version if `TEST` mode is off
-    Asset::set('index.min.css', 20);
+    \Asset::set('index.min.css', 20);
 }
 
-// Create site link data to be used in navigation
-lot('links', new Anemone((static function ($links, $state, $url) {
-    $index = LOT . D . 'page' . D . trim(strtr($state->route ?? 'index', '/', D), D) . '.page';
-    $route = $url->path . '/';
-    foreach (g(LOT . D . 'page', 'page') as $k => $v) {
-        // Exclude home page
-        if ($k === $index) {
-            continue;
-        }
-        $v = new Page($k);
-        // Add current state
-        $v->current = 0 === strpos($route, $v->route . '/');
-        $links[$k] = $v;
-    }
-    ksort($links);
-    return $links;
-})([], $state, $url)));
-
-// Create site trace data to be used in navigation
-lot('traces', new Pages((static function ($traces, $state, $url) {
-    $chops = explode('/', trim($url->path ?? "", '/'));
-    $v = LOT . D . 'page';
-    while ($chop = array_shift($chops)) {
-        $v .= D . $chop;
-        if ($file = exist([
-            $v . '.archive',
-            $v . '.page'
-        ], 1)) {
-            $traces[] = $file;
-        }
-    }
-    return $traces;
-})([], $state, $url)));
+\lot('links', new \Anemone(links(\LOT . \D . 'page')));
+\lot('traces', new \Pages(traces(\LOT . \D . 'page')));
