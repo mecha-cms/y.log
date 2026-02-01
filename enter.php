e488c1d20491b951b4c83273607433ae380157ec
Taufik Nurrohman <hi@taufik-nurrohman.com>
2026-02-01 11:47:01 +0700

    Update

--- enter.php
+++ enter.php
@@ -6,10 +6,6 @@
     <?php if ($w = w($page->description ?? $site->description ?? "")): ?>
       <meta content="<?= $w; ?>" name="description">
     <?php endif; ?>
-    <?php if ('archive' === $page->x): ?>
-      <!-- Prevent search engines from indexing pages with `archive` state -->
-      <meta content="noindex" name="robots">
-    <?php endif; ?>
     <meta content="<?= eat($page->author); ?>" name="author">
     <title>
       <?= w($t->reverse); ?>
